a0350268b011ee4195bc11fc1012e520cab5e50a
mickew <micke@promarmic.se>
2015-11-18 15:36:56 +0100

    Added MVC

--- src/InfoDisplayWeb/Startup.cs
+++ src/InfoDisplayWeb/Startup.cs
@@ -13,12 +13,15 @@ namespace InfoDisplayWeb
         // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
         public void Configure(IApplicationBuilder app)
         {
             // Add the platform handler to the request pipeline.
             app.UseIISPlatformHandler();
+
+            app.UseMvc();
 
             app.Run(async (context) =>
             {
