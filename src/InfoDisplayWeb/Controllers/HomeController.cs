42bff5f587fae8b2b0f74227a6227f325c3d7c82
mickew <micke@promarmic.se>
2015-11-24 08:04:19 +0100

    Updated to asp.net 5 rc1-final

--- src/InfoDisplayWeb/Controllers/HomeController.cs
+++ src/InfoDisplayWeb/Controllers/HomeController.cs
@@ -1,16 +1,26 @@
-﻿using Microsoft.AspNet.Mvc;
+﻿using Microsoft.AspNet.Authorization;
+using Microsoft.AspNet.Mvc;
 
 // For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace InfoDisplayWeb.Controllers
 {
+    [Authorize]
     [Route("[controller]"), Route("/")]
     public class HomeController : Controller
     {
+        [AllowAnonymous]
         [Route("[action]"), Route("")]
         public IActionResult Index()
         {
             return View();
+        }
+
+        [AllowAnonymous]
+        [Route("[action]")]
+        public IActionResult Error()
+        {
+            return View("~/Views/Shared/Error.cshtml");
         }
     }
 }
--- src/InfoDisplayWeb/Startup.cs
+++ src/InfoDisplayWeb/Startup.cs
@@ -1,19 +1,60 @@
 ﻿using Microsoft.AspNet.Builder;
+using Microsoft.AspNet.Diagnostics.Entity;
+using Microsoft.AspNet.Hosting;
 using Microsoft.AspNet.Http;
-using Microsoft.Framework.DependencyInjection;
+using Microsoft.Dnx.Runtime;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.PlatformAbstractions;
 
 namespace InfoDisplayWeb
 {
     public class Startup
     {
+        public Startup(IHostingEnvironment env, IApplicationEnvironment appEnv)
+        {
+            // Setup configuration sources.
+
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(appEnv.ApplicationBasePath)
+                .AddJsonFile("appsettings.json")
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true);
+
+            if (env.IsDevelopment())
+            {
+                // This reads the configuration keys from the secret store.
+                // For more details on using the user secret store see http://go.microsoft.com/fwlink/?LinkID=532709
+                builder.AddUserSecrets();
+            }
+            builder.AddEnvironmentVariables();
+            Configuration = builder.Build();
+        }
+        public IConfigurationRoot Configuration { get; set; }
+
         // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
         }
 
-        public void Configure(IApplicationBuilder app)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+            // Add the following to the request pipeline only in development environment.
+            if (env.IsDevelopment())
+            {
+                app.UseBrowserLink();
+                app.UseDeveloperExceptionPage();
+                app.UseDatabaseErrorPage(options =>
+                {
+                    options.EnableAll();
+                });
+            }
+            else
+            {
+                // Add Error handling middleware which catches all application specific errors and
+                // sends the request to the following path or controller action.
+                app.UseExceptionHandler("/Home/Error");
+            }
             // Add the platform handler to the request pipeline.
             app.UseIISPlatformHandler();
             
@@ -21,10 +62,8 @@ namespace InfoDisplayWeb
 
             app.UseMvc();
 
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
-            });
         }
+
+        public static void Main(string[] args) => WebApplication.Run<Startup>(args);
     }
 }
